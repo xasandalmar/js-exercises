52f075b53e9722cd820488a5fe52c0eac6289f17
xasandalmar <xasandalmar@gmail.com>
2026-06-27 17:27:34 +0300

    Exercise 3

---
+++ Exercise 3/main.js
@@ -0,0 +1,9 @@
+// Variable declarations
+let isSunny = true;
+let isWeekend = true;
+
+// Logical AND (&&) - Both conditions must be true to go to the beach
+let canGoToBeach = isSunny && isWeekend;
+
+// Log the result
+console.log("Can I go to the beach?", canGoToBeach); // Output: true
