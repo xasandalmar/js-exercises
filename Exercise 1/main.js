148a7c89d28faad6432d2120f62030e022af6035
xasandalmar <xasandalmar@gmail.com>
2026-06-27 10:15:47 +0300

    Submit Js Exercise

---
+++ Exercise 1/main.js
@@ -0,0 +1,10 @@
+let x = 7;
+let y = 3;
+
+console.log(x + y);  // Output: 10
+console.log(x - y);  // Output: 4
+console.log(x * y);  // Output: 21
+console.log(x / y);  // Output: 2.3333333333333335
+console.log(x % y);  // Output: 1
+console.log(x++);    // Output: 7 (then x becomes 8)
+console.log(x--);    // Output: 8 (then x becomes 7)
