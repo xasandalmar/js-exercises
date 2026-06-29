099ef3f3aea430658e93d57003547e9eb11f9450
xasandalmar <xasandalmar@gmail.com>
2026-06-29 10:29:20 +0300

    exercise 4

---
+++ Exerice 4/main.js
@@ -0,0 +1,24 @@
+
+    // Fuction Returning a value
+
+function add(a, b) {
+    return a + b;
+}
+let result = add(10, 20);
+console.log("The result is: " + result / 2);
+let result2 = add(30, 40);
+console.log("The result is: " + result2 / 2);
+let result3 = add(50, 60);
+console.log("The result is: " + result3 / 2);
+let result4 = add(70, 80);
+console.log("The result is: " + result4 / 2);
+
+
+// finction expression
+
+const greet = function(name) {
+    console.log("Hello " + name + "!");
+}
+greet("Mohamed Hassan");
+greet("Mumtaz Hassan ");
+greet("Ahmd Hassan");
