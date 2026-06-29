ff4c09be958490f223af420ab63282ab25b540d8
xasandalmar <xasandalmar@gmail.com>
2026-06-29 18:40:01 +0300

    Excercise 9 Methods in objects

---
+++ Exercise 9 Methods in Objects/main.js
@@ -0,0 +1,18 @@
+let car ={
+    Make : "2020",
+    Model :"Camry",
+    Year : "2025",
+    start: function() {
+            console.log("the Car Started " + this.Make);
+        }
+};
+
+console.log(car);
+console.log(car.Make);
+console.log(car.Model);
+console.log(car.Year);
+
+
+console.log(car.Make);
+
+console.log(car.start());
