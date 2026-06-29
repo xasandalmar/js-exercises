15871cced13537639c0d51e670e23b0726428d82
xasandalmar <xasandalmar@gmail.com>
2026-06-29 19:42:01 +0300

    Exercsie 10 Arrays of Objects

---
+++ Exercise 10 Arrays of Objects/main.js
@@ -0,0 +1,11 @@
+
+
+let library = [
+    
+        {title: "Dhaxal Reeb",  author: " Yusuf Ali Ahmed ", year: 1925},
+        {title:"Xasuus Qor", author:"Ibrahim Warsame Dahir", yaer: 2020},
+        {title: "Diyaal Reeb", author:"Khalid Jamac Abdi", year:2001}
+
+];
+    console.log(library[0].title);
+    console.log(library[1].author);
