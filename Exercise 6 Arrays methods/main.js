7a4baed7ec0873ca3d2fd42a7d7faac8f7bf8646
xasandalmar <xasandalmar@gmail.com>
2026-06-29 17:33:42 +0300

    Exercise 7 Arrays methods

---
+++ Exercise 6 Arrays methods/main.js
@@ -0,0 +1,10 @@
+let colors = ["red", "green", "blue"];
+
+    // push - add
+colors.push("yellow");
+console.log(colors);
+
+
+    //shift - Romove the first color
+colors.shift()
+console.log(colors)
