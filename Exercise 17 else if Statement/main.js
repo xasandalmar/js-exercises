ebd925f2b12cf3db0bb829624f43e8849de9d1bd
xasandalmar <xasandalmar@gmail.com>
2026-06-30 11:12:06 +0300

    Exercise 17 else if Statement

---
+++ Exercise 17 else if Statement/main.js
@@ -0,0 +1,14 @@
+
+let temperature =15;
+if (temperature<5){
+    console.log("Very Cold")
+}
+else if (temperature < 15){
+    console.log("cold")
+}
+else if (temperature < 25){
+    console.log("Warm")
+}
+ else{
+    console.log("hot")
+ }
