ed9bafda9c98b5f0ad74af97c751d30189b92532
xasandalmar <xasandalmar@gmail.com>
2026-07-04 18:09:57 +0300

    Exercise 23

---
+++ Exercise 23 Object Destructuring/main.js
@@ -0,0 +1,7 @@
+const car = { make: "Toyota", model: "Corolla", year: 2020 };
+
+// Halkan waa destructuring-kii
+const { make, model } = car;
+
+console.log(make);  // Waxay soo saari doontaa: Toyota
+console.log(model); // Waxay soo saari doontaa: Corolla
